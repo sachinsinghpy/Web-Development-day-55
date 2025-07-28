92379ad7494c83da2fd53a17eb3984286d3eb8f6
Sachin Kumar <skraja012345678@gmail.com>
2025-07-28 19:39:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Hey this is tutorial 55")
+
+var a = 5;
+var b = 6;
+console.log(a+b);
+var c = "Sachin";
+console.log(typeof(c));
+
+console.log(typeof a , typeof b , typeof c);
+
+let x = "Sachin";
+let y = "22";
+let z = 34.43;
+let p = true;
+let q = null;
+let r = undefined;
+
+console.log(x , y, z , p ,q,r);
+
+console.log(typeof x , typeof y, typeof z , typeof p ,typeof q,typeof r);
+
+
+o = {
+    name : "Sachin",
+    "Job code":5600,
+
+
+}
+
+console.log(o);
+
+console.log(typeof(o));
+
+o.salary = "100 crores";
+console.log(o);
